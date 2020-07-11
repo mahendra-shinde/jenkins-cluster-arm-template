d168567025e2910cc2d1c1ec5989240561b69b66
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-07-11 22:50:41 +0530

    jenkins-user

--- scripts/install-slave.sh
+++ scripts/install-slave.sh
@@ -1,6 +1,6 @@
 apt update -y
 ## Install JDK8, Git & Maven
-apt install openjdk-8-jdk git maven -y
+apt install openjdk-8-jdk git maven openssl -y
 ## Install Docker-CE
 apt install docker.io -y
 ## allow current user to access docker daemon
@@ -10,3 +10,5 @@ wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scann
 unzip sonar-scanner-cli-4.2.0.1873-linux.zip
 mv sonar-scanner-4.2.0.1873-linux/ sonar/
 rm sonar-scanner-cli-4.2.0.1873-linux.zip
+## create new jenkins user with password pass@12345
+sudo useradd -p $(openssl passwd -1 "pass@12345") jenkins
